abf487f758c8e1a72bbc24779d5c6274f70aac14
lrq <lrq15011028264@163.com>
2022-02-22 17:52:38 +0800

    返回简单的HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -9,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  # ➊
+        response = home_page(request)  # ➋
+        self.assertTrue(response.content.startswith(b'<html>'))  # ➌
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  # ➍
+        self.assertTrue(response.content.endswith(b'</html>'))  # ➎
