50764e8063c4a3217a44cf1b4ba2b19b4fa7dc8f
lrq <lrq15011028264@163.com>
2022-02-22 16:56:37 +0800

    使用unittest编写的收个功能测试

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path=r'/home/tarena/桌面/web/chromedriver')
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome(executable_path=r'/home/tarena/桌面/web/chromedriver')
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('to_do', self.browser.title)
+        self.fail('finish the test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
