8b5dbf7bbd83476fdf76eaf70520ea523829bca3
lrq <lrq15011028264@163.com>
2022-02-22 18:20:24 +0800

    function_test的改动

--- functional_tests.py
+++ functional_tests.py
@@ -1,17 +1,36 @@
 from selenium import webdriver
 import unittest
 
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Chrome(executable_path=r'/home/tarena/桌面/web/chromedriver')
+        self.browser.implicitly_wait(3)
 
     def tearDown(self) -> None:
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('to_do', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
+        # 标题和头部包含To-Do
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # 查找一个html的ID值
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        # 向文本框输入值
+        inputbox.send_keys('Buy peacock feathers')
+        # 输入回车确认
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         self.fail('finish the test')
 
 
